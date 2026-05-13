7b6c244ce9447ce6f4ebb4b1d27d80d323dc99c9
Julieta Pellettieri <117550230+JPellettieri@users.noreply.github.com>
2026-05-13 14:56:39 -0300

    VivovsVitro

--- Correlaciones.R
+++ Correlaciones.R
@@ -31,12 +31,7 @@ res <- rcorr(as.matrix(D_num), type = "spearman")
 res
 cor_mat <- res$r      # correlaciones
 p_mat   <- res$P      # p-values
-#para el coorplot
- # p_filtrada <- p_mat
- # p_filtrada[p_filtrada >= 0.1] <- NA
- # orden_vars <- c(   "Frec",   "Int",   "Arb",   "Vesic",   "EsporasS",   "EsporasMM",   "EsporasIntraRad",   "MicelioMM",   "MicelioS", "PSuelo", "CuSuelo", "Fe","gt","gfe" )
- # p_filtrada <- p_filtrada[orden_vars, orden_vars]
- 
+
 threshold_r <- 0.2 #la correlacion minma necesaria para que se grafique en el mapa
 threshold_p <- 0.05   # p valor marginal como minimo
 
@@ -92,7 +87,7 @@ library(dplyr)
 
 D_VIVO <- DvivoInc[, sapply(DvivoInc, is.numeric)]
 D_VIVO <- D_VIVO[, colSums(is.na(D_VIVO)) < nrow(D_VIVO)] # eliminar columnas completamente vacías
-D_VIVO <- subset(D_VIVO, select = -c(EE, `largo vasta`, id, `pf raiz`, `pf aer`,`RAIZ+TIERRA`,PStotal ))# eliminar columnas `largo vasta`, id, `pf raiz`, `pf aer`,`RAIZ+TIERRA`,PStotal  BCRaiz,BCTallo
+D_VIVO <- subset(D_VIVO, select = -c(id,PStotal,`largo vasta`, id, `pf raiz`, `pf aer`,`RAIZ+TIERRA`,PStotal))# eliminar columnas `largo vasta`, id, `pf raiz`, `pf aer`,`RAIZ+TIERRA`,PStotal 
 colnames(D_VIVO) <- make.names(trimws(colnames(D_VIVO)))
 res <- rcorr(as.matrix(D_VIVO), type = "spearman")
 res
@@ -101,20 +96,6 @@ p_mat   <- res$P      # p-values
 
 threshold_r <- 0.2
 threshold_p <- 0.05   # p valor marginal como minimo
-#para el coorplot
-p_filtradavivo <- p_mat
-p_filtradavivo[p_filtradavivo >= 0.1] <- NA
-orden_vars <- c(
-  "CuSuelo","PSuelo",
-  "CuRaiz","PRaiz","BCRaiz" , "BCTallo",
-  "CuVastago","PVastago",
-  "FT","Frec","Int",
-  "Lhif","gt","gfe",
-  "adherido",
-  "p.seco.raiz",
-  "p.s.aereo"
-)
-p_filtradavivo <- p_filtradavivo[orden_vars, orden_vars]
 
 valid <- abs(cor_mat) > threshold_r & p_mat < threshold_p
 #Filtro la matriz
@@ -153,7 +134,7 @@ ggraph(g, layout = "fr") +
   geom_edge_link(aes(edge_width = abs(value), repel = TRUE,
                      color = value > 0),
                  alpha = 0.8) +
-  #geom_edge_parallel(aes(colour = year)) colour= tratamiento de cobre
+  
   geom_edge_link(aes(label = round(value, 2)),
                  angle_calc = 'along',
                  label_dodge = unit(2.5, 'mm')) +
@@ -171,63 +152,8 @@ ggraph(g, layout = "fr") +
 
   
 
+#geom_edge_parallel(aes(colour = year)) colour= tratamiento de cobre
 
 
 
-
-###############
-# Paquetes
-# install.packages(c("igraph", "ggraph", "tidygraph", "reshape2"))
-library(igraph)
-library(ggraph)
-library(tidygraph)
-library(reshape2)
-
-# 1. Matriz de correlación
-cor_VRVivo
-cor_VRVitro
-# 1. Asegurar matriz numérica limpia
-#cor_mat <- as.matrix(cor_VRVivo)
-cor_mat <- as.matrix(cor_VRVitro)
-# 2. Eliminar posibles NA (clave)
-cor_mat[is.na(cor_mat)] <- 0
-
-# 3. Formato largo
-df_cor <- melt(cor_mat)
-
-# 4. Convertir a character (evita error de factores)
-df_cor$Var1 <- as.character(df_cor$Var1)
-df_cor$Var2 <- as.character(df_cor$Var2)
-
-# 5. Filtrar
-threshold <- 0.6   # te recomiendo más estricto para este dataset
-
-df_cor <- subset(df_cor, Var1 != Var2 & abs(value) > threshold)
-
-# 6. Eliminar duplicados
-df_cor <- df_cor[df_cor$Var1 < df_cor$Var2, ]
-
-# 7. Crear grafo
-g <- graph_from_data_frame(df_cor, directed = FALSE)
-
-# 8. Agrupación ecológica
-V(g)$grupo <- ifelse(grepl("Suelo", V(g)$name), "Suelo",
-                     ifelse(grepl("Raiz", V(g)$name), "Raíz",
-                            ifelse(grepl("Vastago|Tallo|aereo", V(g)$name), "Parte aérea",
-                                   "Fisiológico")))
-
-# 9. Plot
-ggraph(g, layout = "fr") +
-  geom_edge_link(aes(edge_alpha = abs(value),
-                     edge_width = abs(value),
-                     color = value > 0)) +
-  geom_edge_link(aes(label = round(value, 2)),
-                 angle_calc = 'along',
-                 label_dodge = unit(2.5, 'mm')) +
-  
-  geom_node_point(aes(color = grupo), size = 6) +
-  geom_node_text(aes(label = name), repel = TRUE, size = 4) +
-  scale_edge_color_manual(values = c("red", "blue")) +
-  scale_edge_width(range = c(0.5, 2)) +
-  theme_void()
 
